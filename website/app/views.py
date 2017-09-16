41ab8954c9c83b234febb2e6a45fd86805b78b5b
cri-zrh <cri-zrh>
2017-09-16 03:00:34 +0200

    fix path of files directory

--- website/app/views.py
+++ website/app/views.py
@@ -28,5 +28,13 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('upload_file',
                                     filename=filename))
-    return render_template('upload.html')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
--- website/config.py
+++ website/config.py
@@ -1,3 +1,3 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'glasgowrangers'
-UPLOAD_FOLDER = '/files'
+UPLOAD_FOLDER = 'files/'
